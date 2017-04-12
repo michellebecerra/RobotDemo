6fa767a6c51801db670f78c1fa45ea5b4b5d0489
michellebecerra <michel8@vt.edu>
2017-04-12 14:37:51 -0700

    demo works

--- cordial_example/speech/gen_audio.sh
+++ cordial_example/speech/gen_audio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf data phrases.yaml
 mkdir data
 rosrun cordial_tts gen_phrases.py Justin GDNAJYDWFVWE7HEH4ZLA 2JyYsTIU4apa3xFVj2rAJQxQmaixhwZtv2V+/pII data phrases.yaml script.txt
 oggdec data/*.ogg
